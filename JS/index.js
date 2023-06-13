b0c342155fa42b74a89d085f6a3fba679679489d
Soll0702 <solange.martinez@edurenca.cl>
2023-06-13 16:02:34 -0400

    ok

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+function crearArreglo(num) {        
+        var nuevoArreglo = [];       
+        for (var i = 0; i <= num; i++) {                
+            nuevoArreglo.push(i);        
+        }
+    }
+    creaeArreglo(5);
+    
+    /
+
+
+
+
+
